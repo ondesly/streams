8cd3fa09c0e9c32b0e750c8678d11aa2fe9b570c
ondesly <dmitriytorkhov@gmail.com>
2020-11-10 12:46:16 +0300

    cmake files added

---
+++ cmake/FindMiniz.cmake
@@ -0,0 +1,16 @@
+#
+#  FindMiniz.cmake
+#  zstream
+#
+#  Created by Dmitrii Torkhov <dmitriitorkhov@gmail.com> on 07.11.2020.
+#  Copyright © 2020 Dmitrii Torkhov. All rights reserved.
+#
+
+add_library(miniz STATIC
+	${miniz_SOURCE_DIR}/miniz.c)
+
+add_library(richgel999::miniz ALIAS miniz)
+
+target_include_directories(miniz PUBLIC
+	$<BUILD_INTERFACE:${miniz_SOURCE_DIR}>
+	$<INSTALL_INTERFACE:include/miniz>)
